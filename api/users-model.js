2d9ec7fe8489d25d31d43852c0c6a0d993fbf0fd
Mohamed essamaali <mohamed.essamaali@gmail.com>
2021-01-28 19:06:05 -0500

    register endpoint completed

---
+++ api/users-model.js
@@ -0,0 +1,23 @@
+const db = require('../data/config-db')
+
+const find = ()=>{
+    return db('users')
+}
+const findById = id=>{
+    return db('users').where('id',id).first()
+}
+
+const findBy = credentials=>{
+    return db('users').where(credentials).select('username','password')
+}
+
+const create = credentials=>{
+    return db('users').insert(credentials).then(ids=>findById(ids[0]))
+}
+
+const remove = (id)=>{
+return db('users').del().where('id',id)
+}
+
+
+module.exports={find,findBy,create,remove,findById}
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const usersRoute = require('./api/usersRoute')
 
 const server = express()
 server.use(express.json())
+
+server.use(usersRoute)
 
 
 module.exports = server
