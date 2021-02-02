b205840d04c8d6db4efc3dac367ba118ad0d2420
Mohamed-Essamaali <Mohamed-Essamaali@lambdastudents.com>
2021-02-01 22:48:05 -0500

    t1

--- knexfile.js
+++ knexfile.js
@@ -25,10 +25,10 @@ module.exports = {
 		},
 		migrations: {
 			directory: './data/migrations',
-    },
-    seeds: {
-      directory:  './data/seeds'
     }
+    // seeds: {
+    //   directory:  './data/seeds'
+    // }
 	},
 
 
